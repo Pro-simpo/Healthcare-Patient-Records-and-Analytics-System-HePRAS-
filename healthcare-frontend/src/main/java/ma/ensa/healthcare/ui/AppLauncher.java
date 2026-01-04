14d8efbc2cf748fc93a6080740009e0d13861bd4
Pro-simpo <simporetaobata@gmail.com>
2026-01-04 01:50:12 +0100

    ajout

---
+++ healthcare-frontend/src/main/java/ma/ensa/healthcare/ui/AppLauncher.java
@@ -0,0 +1,8 @@
+package ma.ensa.healthcare.ui;
+
+public class AppLauncher {
+    public static void main(String[] args) {
+        // Appelle la méthode main de votre classe JavaFX existante
+        MainApp.main(args);
+    }
+}
